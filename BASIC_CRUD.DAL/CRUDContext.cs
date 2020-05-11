a30bcd42c61c75381c0bd6dad5ec7fcb80acbb3b
musheerahsan5 <65181542+musheerahsan5@users.noreply.github.com>
2020-05-11 10:12:03 -0700

    Add files via upload

---
+++ BASIC_CRUD.DAL/CRUDContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BASIC_CRUD.Entity;
+using System.Data.Entity;
+
+namespace BASIC_CRUD.DAL
+{
+    public class CRUDContext : DbContext
+    {
+        public CRUDContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Category> categoryList { get; set; }
+
+        public DbSet<Product> productDetails { get; set; }
+    }
+}
---
+++ BASIC_CRUD_DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASIC_CRUD_DAL
+{
+    public class Class1
+    {
+    }
+}
